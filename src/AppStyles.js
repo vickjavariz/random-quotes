e1348345f41d6cccff843f5646103f2efc99aac0
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2020-05-21 17:28:03 -0400

    Add media query breakpoints

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -21,6 +21,19 @@ export const MainDivFunc = (color) => {
     color: ${color};
     padding: 3% 0;
     border-radius: 5px;
+
+    @media (max-width: 992px) {
+      width: 60vw;
+    }
+
+    @media (max-width: 768px) {
+      width: 75vw;
+    }
+
+    @media (max-width: 576px) {
+      width: 90vw;
+      padding: 5% 0;
+    }
   `;
 };
 
@@ -32,6 +45,10 @@ export const ButtonsDiv = styled.div`
   width: 80%;
   margin-top: 20px;
   font-size: 1.2rem;
+
+  @media (max-width: 414px) {
+    flex-direction: column;
+  }
 `;
 
 export const QuoteText = styled.p`
@@ -51,6 +68,18 @@ export const AuthorText = styled.p`
   padding-top: 5px;
   padding-left: 50%;
   text-align: justify;
+
+  @media (max-width: 576px) {
+    padding-left: 40%;
+  }
+
+  @media (max-width: 414px) {
+    padding-left: 30%;
+  }
+
+  @media (max-width: 380px) {
+    padding-left: 25%;
+  }
 `;
 
 export const NewQuoteButtonFunc = (color) => {
@@ -61,9 +90,20 @@ export const NewQuoteButtonFunc = (color) => {
     border-radius: 4px;
     cursor: pointer;
     padding: 13px 18px;
+    font-size: 0.9rem;
 
     &:hover {
       opacity: 0.8;
+    }
+
+    @media (max-width: 414px) {
+      margin-bottom: 20px;
+      padding: 15px 30%;
+      font-size: 1.1rem;
+    }
+
+    @media (max-width: 380px) {
+      font-size: 1rem;
     }
   `;
 };
@@ -78,6 +118,10 @@ export const TwitterAnchorFunc = (color) => {
 
     &:hover {
       opacity: 0.8;
+    }
+
+    @media (max-width: 414px) {
+      padding: 15px 42%;
     }
   `;
 };
